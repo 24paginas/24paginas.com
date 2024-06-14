c191f3bea097cc798b8f6d1061ddf63127563a66
neverbot <ivan@neverbot.com>
2024-06-14 22:25:38 +0200

    fix: in galleries script, do not parse post content with yq

--- web/source/galleries.sh
+++ web/source/galleries.sh
@@ -9,7 +9,7 @@ for file in $(find . -type f); do
     awk '/^\---$/ { if(++delim % 2 == 0) { next } } { file = sprintf("chunk%s.txt", int(delim / 2)); print > file; }' < $file
 
     # brew install yq!!
-    LINE=`yq '.media[]' $file | awk -v d='" "' '{s=(NR==1?s:s d)$0}END{print s}'`
+    LINE=`yq '.media[]' chunk0.txt | awk -v d='" "' '{s=(NR==1?s:s d)$0}END{print s}'`
 
     echo "{% gallery \"$LINE\" %}"
 
